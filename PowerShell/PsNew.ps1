8ec85249d772a1aad79f7e591803969d899a6109
JordanSanders-Devart <62429421+JordanSanders-Devart@users.noreply.github.com>
2021-12-09 09:21:53 +0200

    Delete PsNew.ps1

--- PowerShell/PsNew.ps1
+++
@@ -1,7 +0,0 @@
-﻿$ServerName = "Deniss\SQL2019ENT"
-$DatabaseName = "TestDB"
-
-# Create a database connection
-Write-Host "Creating a database connection..." -ForegroundColor Cyan
-$Connection = New-DevartSqlDatabaseConnection -Server $ServerName -database $DatabaseName -UserName SA -Password 123
-
